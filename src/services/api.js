a233cd5cfaee08bfe25a394ce66cbc01e91c8a19
“[Prachi <[rprachi518@gmail.com]>
2026-04-02 22:38:43 +0530

    your message

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://job-portal-backend-ey3y.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL
 });
-
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
     if (token) {
